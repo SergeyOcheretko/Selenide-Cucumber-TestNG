cfc0794f7dee16d4aede6dfef62592c6954edb9c
SergeyOcheretko <sergej.ocheretko1@gmail.com>
2023-02-20 18:12:15 +0200

    Commit 4

--- src/test/java/RunCucumberTests.java
+++ src/test/java/RunCucumberTests.java
@@ -1,7 +1,8 @@
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import net.thucydides.core.annotations.Feature;
 
-
+@Feature
 @CucumberOptions(
         features = {"C:\\Users\\Sergey\\IdeaProjects\\Selenide-Cucumber-TestNG\\src\\test\\resources\\feature"},
         glue = {"settings"})
